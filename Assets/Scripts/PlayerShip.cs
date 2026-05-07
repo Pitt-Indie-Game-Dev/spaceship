7763195c7809fa67c4abb416a9789b076c7026f6
Samuel Kuhns <70383095+pippinsims@users.noreply.github.com>
2026-05-06 23:48:02 -0400

    feat: Add a rough base for UI work

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -1,74 +1,47 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
-using Unity.Properties;
-using UnityEditor.Tilemaps;
-using UnityEngine.UIElements;
 
 [RequireComponent (typeof (Rigidbody2D))]
 public class PlayerShip : MonoBehaviour
 {
-    
-    public float speed = 5.0f; // Speed of ship at full throttle
+    public float speed = 5.0f;                  // Speed of ship at full throttle
     public float directionAdjustSpeed = 180.0f; // How fast direction changes (degrees/s)
-    public float throttleAdjustSpeed = 1.0f; // How fast throttle changes (1.0 = 1.0 seconds from 0-100%)
-    //public TMP_Text speedUI;
-
-    [SerializeField]
+    public float throttleAdjustSpeed = 1.0f;    // How fast throttle changes (1.0 = 1.0 seconds from 0-100%)
+    
+    public int minimapZoomLevel = 1;            //TODO ideally there'd be a HUD script for this...
+    
+    [SerializeField] 
     private Transform cameraTransform;
     
-    private float _direction;
-
-    
-    
-    private float _throttle;
-    [CreateProperty]
-    public float Throttle
-    {
-        get => _throttle;
-        set => _throttle = value;
-    }
-
-    [SerializeField]
-    private UIDocument uiDocument;
-
     private Rigidbody2D _rigidbody;
+    
+    private float _direction;
+    private float _throttle;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        var root = FindAnyObjectByType<UIDocument>().rootVisualElement;
-        var label = root.Q<Label>("Velocity");
-        label.SetBinding("text", new DataBinding()
-        {
-            dataSource = this,
-            dataSourcePath = new PropertyPath(nameof(Throttle))
-        });
     }
 
     // Update is called once per frame
     private void Update()
     {
         cameraTransform.position = new Vector3(
-            transform.position.x, transform.position.y,
+            transform.position.x, 
+            transform.position.y,
             cameraTransform.position.z // Preserve camera depth
         );
-        //speedUI.text = "V: " + speed.ToString();
     }
 
     // FixedUpdate is called once per physics step
     private void FixedUpdate()
     {
-        if (Keyboard.current.wKey.isPressed)
-            _throttle += throttleAdjustSpeed * Time.deltaTime;
-        if (Keyboard.current.sKey.isPressed)
-            _throttle -= throttleAdjustSpeed * Time.deltaTime;
-        if (Keyboard.current.aKey.isPressed)
-            _direction += directionAdjustSpeed * Time.deltaTime;
-        if (Keyboard.current.dKey.isPressed)
-            _direction -= directionAdjustSpeed * Time.deltaTime;
+        if (Keyboard.current.wKey.isPressed) _throttle += throttleAdjustSpeed * Time.deltaTime;
+        if (Keyboard.current.sKey.isPressed) _throttle -= throttleAdjustSpeed * Time.deltaTime;
+        
+        if (Keyboard.current.aKey.isPressed) _direction += directionAdjustSpeed * Time.deltaTime;
+        if (Keyboard.current.dKey.isPressed) _direction -= directionAdjustSpeed * Time.deltaTime;
         
         _throttle = Mathf.Clamp(_throttle, 0.0f, 1.0f);
         
---
+++ Assets/Scripts/RuntimeUI.cs
@@ -0,0 +1,151 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
+
+[RequireComponent (typeof (UIDocument))]
+public class RuntimeUI : MonoBehaviour
+{
+    public VisualTreeAsset pointerTemplate;
+
+    [SerializeField] 
+    private PlayerShip ship;
+    private Rigidbody2D _srb;
+
+    private Label _x;
+    private Label _y;
+    private Label _speed;
+    private Label _zoom;
+    private Image _rspin;
+    
+    private VisualElement _pointers;
+    private VisualElement _radar;
+    private VisualElement _numberreadings;
+    private VisualElement _velocitybars;
+    private Image _compass;
+    private VisualElement _velocitygrid;
+    private Image _xvelgrid;
+    private Image _yvelgrid;
+    
+    private float _startTime;
+    private int _curBlink = 1;
+
+    private readonly List<Arrow> _arrows = new List<Arrow>();
+    private List<Image> divisionLine = new List<Image>();
+
+    private void OnEnable()
+    {
+        _srb = ship.GetComponent<Rigidbody2D>();
+        
+        VisualElement r = GetComponent<UIDocument>().rootVisualElement;
+        _zoom  = r.Q<Label>("zoomLabel");
+        _x     = r.Q<Label>("xposLabel");
+        _y     = r.Q<Label>("yposLabel");
+        _speed = r.Q<Label>("speedLabel");
+        _rspin = r.Q<Image>("radarspin");
+
+        _pointers       = r.Q("Pointers");
+        _radar          = r.Q("Radar");
+        _numberreadings = r.Q("NumberReadings");
+        _velocitybars   = r.Q("VelocityBars");
+        _compass        = r.Q<Image>("Compass");
+        _velocitygrid   = r.Q("VelocityGrid");
+        _xvelgrid       = r.Q<Image>("RedGridMarker");
+        _yvelgrid       = r.Q<Image>("GreenGridMarker");
+
+        _pointers      .visible = false;
+        _radar         .visible = false;
+        _numberreadings.visible = false;
+        _velocitybars  .visible = false;
+        _compass       .visible = false;
+        _velocitygrid  .visible = false;
+
+        _arrows.Add(new Arrow(GameObject.Find("Asteroid"    ).transform, ship.transform, pointerTemplate, _pointers));
+        _arrows.Add(new Arrow(GameObject.Find("Asteroid (1)").transform, ship.transform, pointerTemplate, _pointers));
+
+        divisionLine.Add(r.Q<Image>("DL1")); SetWidth (divisionLine[0], Length.Percent(0));
+        divisionLine.Add(r.Q<Image>("DL2")); SetHeight(divisionLine[1], Length.Percent(0));
+        divisionLine.Add(r.Q<Image>("DL3")); SetWidth (divisionLine[2], Length.Percent(0));
+        divisionLine.Add(r.Q<Image>("DL4")); SetHeight(divisionLine[3], Length.Percent(0));
+        divisionLine.Add(r.Q<Image>("DL5")); SetWidth (divisionLine[4], Length.Percent(0));
+
+        _startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        _zoom .text = "Z: " + ship.minimapZoomLevel;
+        _x    .text = "X: " + ship.transform.position.x.ToString("N0");
+        _y    .text = "Y: " + ship.transform.position.y.ToString("N0");
+        _speed.text = "V: " + _srb.linearVelocity.magnitude.ToString("0.000");
+        _rspin.style.rotate = new Rotate(new Angle(_rspin.style.rotate.value.angle.value + 1));
+
+        _xvelgrid.style.left = Length.Percent(       50 + _srb.linearVelocity.x/ship.speed * 50 - 5); 
+        _yvelgrid.style.top  = Length.Percent(100 - (50 + _srb.linearVelocity.y/ship.speed * 50) - 5);
+
+        if(_curBlink < 7)
+        {
+            switch (_curBlink++)
+            {
+                case 1: StartCoroutine(Blink(_pointers))      ; break;
+                case 2: StartCoroutine(Blink(_radar))         ; break;
+                case 3: StartCoroutine(Blink(_numberreadings)); break;
+                case 4: StartCoroutine(Blink(_velocitybars))  ; break;
+                case 5: StartCoroutine(Blink(_compass))       ; break;
+                case 6: StartCoroutine(Blink(_velocitygrid))  ; break;
+            }
+        }
+        
+        foreach (var a in _arrows) a.Point();
+
+        if     (Width (divisionLine[0]) < 100) IncWidth (divisionLine[0], 400 * Time.deltaTime);
+        else if(Height(divisionLine[1]) < 100) IncHeight(divisionLine[1], 400 * Time.deltaTime);
+        else if(Width (divisionLine[2]) < 100) IncWidth (divisionLine[2], 100 * Time.deltaTime);
+        else if(Height(divisionLine[3]) < 100) IncHeight(divisionLine[3], 400 * Time.deltaTime);
+        else if(Width (divisionLine[4]) < 100) IncWidth (divisionLine[4], 400 * Time.deltaTime);
+    }
+
+    private float Height(VisualElement v) { return v.style.height.value.value; }
+    private float Width(VisualElement v) { return v.style.width.value.value; }
+    private void SetHeight(VisualElement v, Length a) { v.style.height = a; }
+    private void SetWidth(VisualElement v, Length a) { v.style.width = a; }
+    private void IncHeight(VisualElement v, float a) { SetHeight(v, Length.Percent(Mathf.Min(Height(v) + a,100))); }
+    private void IncWidth(VisualElement v, float a) { SetWidth(v, Length.Percent(Mathf.Min(Width(v) + a,100))); }
+
+    private IEnumerator Blink(VisualElement v)
+    {
+        yield return new WaitForSeconds(0.3f*_curBlink);
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            v.visible = false;
+            yield return new WaitForSeconds(0.1f);
+            v.visible = true;
+        }
+    }
+
+    private class Arrow
+    {
+        private readonly Transform _target;
+        private readonly Transform _ship;
+        private readonly TemplateContainer _pointer;
+        
+        public Arrow(Transform target, Transform ship, VisualTreeAsset defaultPtr, VisualElement parent)
+        {
+            _target = target;
+            _ship = ship;
+            parent.Add(_pointer = defaultPtr.Instantiate());
+            _pointer.style.position = Position.Absolute;
+            _pointer.style.right = 0;
+            _pointer.style.width = Length.Percent(100);
+            _pointer.style.height = Length.Percent(8);
+        }
+
+        public void Point()
+        {
+            Vector3 d = _target.position - _ship.position;
+            _pointer.style.rotate = new Rotate(Angle.Degrees(-Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg));
+        }
+    }
+}
