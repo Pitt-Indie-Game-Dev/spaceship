6450b42243aa1a1dd8587aa0eedd775bb9788b44
JackCarluccio <179883228+JackCarluccio@users.noreply.github.com>
2026-07-23 14:56:46 -0400

    feat: asteroids destroy on collisions

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -5,6 +5,15 @@ public class Asteroid : MonoBehaviour
 
     [SerializeField] private float angularVelocityAddition = 360.0f;
 
+    private Vector2Int _cell;
+    private AsteroidManager _manager;
+
+    public void Init(Vector2Int cell, AsteroidManager manager)
+    {
+        _cell = cell;
+        _manager = manager;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         var ship = other.GetComponent<PlayerShip>();
@@ -20,5 +29,6 @@ public class Asteroid : MonoBehaviour
         }
         
         rb.angularVelocity += angularVelocityAddition;
+        _manager.DeleteAsteroid(_cell);
     }
 }
